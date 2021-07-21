96b66d55fafb042fe35205e33dd6ce68aa5256cb
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-07-21 09:39:14 -0700

    Firebase utils, included storing authenticated users into Firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -17,12 +17,27 @@ class App extends React.Component {
     };
   }
 
-  // set up a subscription to Google Firebase for Auth status changes
+  // set up a subscription to Google Firebase for Auth status changes -- this persists while the client is on our site
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // ignore if the user signed out -- we are alerted both when a user signs in and out. On sign out there is a null userAuth object returned
+      if (userAuth) {
+        // the user profile document is the documentReference to the collection table for the current user
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        // whenever we change the user's data using the userSnapshot update the state of our app to match the user's firestore data
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        // if user is signed out set currentUser to null
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
   measurementId: "G-EML7624QZF",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // no signed in user
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (e) {
+      console.log("error creating user", e.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
