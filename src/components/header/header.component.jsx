72fb13305705de9a0b9bff3f8d582b451b420173
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-07-22 19:42:49 -0700

    cart icon, cart dropdown components added to application. Added reducers and actions.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,15 +4,17 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
         <Logo className="logo"></Logo>
       </Link>
-
       <div className="options">
         <Link to="/shop" className="option">
           SHOP
@@ -29,15 +31,18 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
 // state is the root reducer
-const mapStateToProps = (state) => {
+// take the user reducer from the redux store and get the currentUser value from that
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => {
   // get the user reducer and grab the currentUser value from that
-  return { currentUser: state.user.currentUser };
+  return { currentUser, hidden };
 };
 
 // higher order function that wraps the Header and connects it to the Redux store
